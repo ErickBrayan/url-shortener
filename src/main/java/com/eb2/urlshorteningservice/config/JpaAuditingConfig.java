9093a1fae18202f4857433a917e19e947082515a
ErickBrayan <ebrayan2000@gmail.com>
2025-04-03 18:52:11 -0500

    add auiting

---
+++ src/main/java/com/eb2/urlshorteningservice/config/JpaAuditingConfig.java
@@ -0,0 +1,4 @@
+package com.eb2.urlshorteningservice.config;
+
+public class JpaAuditingConfig {
+}
--- src/main/java/com/eb2/urlshorteningservice/service/KafkaService.java
+++
@@ -1,9 +0,0 @@
-package com.eb2.urlshorteningservice.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class KafkaService {
-
-
-}
