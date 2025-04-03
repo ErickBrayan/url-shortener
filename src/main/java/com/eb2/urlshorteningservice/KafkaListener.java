2a0075bcbb15c302ddbb78b7087427a472eb57e6
ErickBrayan <ebrayan2000@gmail.com>
2025-04-03 18:37:19 -0500

    add kafka service

--- src/main/java/com/eb2/urlshorteningservice/KafkaListener.java
+++ src/main/java/com/eb2/urlshorteningservice/KafkaListener.java
@@ -3,16 +3,16 @@ package com.eb2.urlshorteningservice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-@Component
-@Slf4j
+//@Component
+//@Slf4j
 public class KafkaListener {
 
 
-    @org.springframework.kafka.annotation.KafkaListener(
-            topics = "url",
-            groupId = "groupId"
-    )
-    void listener (String data) {
-        log.info("Listener:  redirect to page by shortCode -> {}", data);
-    }
+//    @org.springframework.kafka.annotation.KafkaListener(
+//            topics = "url",
+//            groupId = "groupId"
+//    )
+//    void listener (String data) {
+//        log.info("Listener:  redirect to page by shortCode -> {}", data);
+//    }
 }
--- src/main/java/com/eb2/urlshorteningservice/service/KafkaService.java
+++ src/main/java/com/eb2/urlshorteningservice/service/KafkaService.java
@@ -1,9 +1,36 @@
 package com.eb2.urlshorteningservice.service;
 
+import com.eb2.urlshorteningservice.entities.Shorten;
+import com.eb2.urlshorteningservice.repository.ShortenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
+@Component
+@RequiredArgsConstructor
 public class KafkaService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ShortenRepository repository;
+
+    @KafkaListener(topics = "url", groupId = "groupId")
+    void listener (String shortCode) {
+        updateStats(shortCode);
+    }
+
+    private void updateStats(String shortCode) {
+
+        repository.findByShortCode(shortCode).ifPresent(s -> {
+            int accessCount = s.getAccessCount();
+            s.setAccessCount(accessCount + 1);
+            repository.save(s);
+        });
+
+    }
+
 
 
 }
